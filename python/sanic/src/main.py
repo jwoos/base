b784b188e8fc14eb83ec4d4be8067393d195ef9e
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-11-15 16:23:16 -0500

    sanic - error handling

--- python/sanic/src/main.py
+++ python/sanic/src/main.py
@@ -1,6 +1,7 @@
 import os
 
 from sanic import Sanic, response
+from sanic.exceptions import SanicException
 
 
 app = Sanic(__name__)
@@ -11,9 +12,19 @@ async def test(request):
     return response.json({'hello': 'world'})
 
 
+@app.exception(SanicException)
+async def sanic_exception_handler(request, exception):
+    return response.json({
+        'error': {
+            'exception': getattr(exception, 'message', None) or str(exception),
+            'status_code': getattr(exception, 'status_code', None),
+        }
+    })
+
+
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=9000,
-        workers=len(os.sched_getaffinity(0)),
+        workers=os.cpu_count(),
     )
